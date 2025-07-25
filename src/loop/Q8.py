ae6cc2bdb92f621a7414ed43184f179c63eaf540
Abhishek Sharma <abhimayank1234@gmail.com>
2025-07-25 13:55:34 +0530

    new file Q8 added with 2 different answers

---
+++ src/loop/Q8.py
@@ -0,0 +1,48 @@
+prime_num= int(input("Write the prime number"))
+a=True
+if prime_num>1:
+    for i in range(2,prime_num):
+        if prime_num%i==0:
+            a=False
+            
+        
+print(a)
+            
+
+
+
+
+
+
+
+
+
+
+#using Function
+
+
+def prime_number(n):
+    if n<=1:
+        return False
+    if n==2:
+        return True
+    if n%2==0:
+        return False
+    for i in range(3,int(n**(0.5)+1),2): # type: ignore
+        if n%i==0:
+          return False
+    return True
+    
+prime_num= float(input("Write the prime number"))
+
+if prime_number(prime_num):
+    print("prime number")
+else:
+    print("not prime number")
+
+
+
+            
+            
+        
+        
