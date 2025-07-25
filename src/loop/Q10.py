f8720dfb0df88b053557edd3dd80a190c3e34bce
Abhishek Sharma <abhimayank1234@gmail.com>
2025-07-25 14:52:49 +0530

    done

---
+++ src/loop/Q10.py
@@ -0,0 +1,12 @@
+import time
+
+
+wait_time =
+max_retries=5
+count=0
+
+while count<max_retries:
+    print("count",count + 1,"wait time" ,wait_time)
+    time.sleep(wait_time)
+    wait_time*=2
+    count+=1
